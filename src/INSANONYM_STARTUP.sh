daef4f23714e5e37113e28f39e6d01e756ae7518
MinPNG <minhphongdn1@yahoo.com>
2024-12-08 15:55:23 +0100

    refresh

--- src/INSANONYM_STARTUP.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /var/www/html
-chown www-data:www-data -R ./
-# chmod 0740 -R ./
-apache2-foreground
--- src/scripts/src/Shuffle.py
+++
@@ -1,9 +0,0 @@
-from pandas.core.frame import DataFrame
-import logging
-
-def shuffleDataFrame(_df: DataFrame):
-    df = _df
-    df = df.astype({'longitude': 'float64', 'latitude': 'float64', 'id': 'string', 'date': 'datetime64[ns]'})
-    shuffled = df.sample(n=len(df))
-    logging.debug("Shuffle finished")
-    return shuffled
